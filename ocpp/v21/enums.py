137acf6380d18aa6df0a2d07655f184ba7a2149d
Ferran Jovell <f.jovell@swishforgood.com>
2026-09-22 14:37:04 +0200

    Ignore no-redef for StrEnum in enums

--- ocpp/v21/enums.py
+++ ocpp/v21/enums.py
@@ -1,13 +1,13 @@
 import sys
 
-if sys.version_info < (3, 11, 0):
-    from enum import Enum
-
-    class StrEnum(str, Enum):
-        pass
+if sys.version_info >= (3, 11, 0):
+    from enum import StrEnum
 
 else:
-    from enum import StrEnum
+    from enum import Enum
+
+    class StrEnum(str, Enum):  # type: ignore [no-redef]
+        pass
 
 
 class Action(StrEnum):
